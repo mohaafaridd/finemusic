2fd603c597eabe6b4394f5d24803ed5dc6dc90ff
Mohammed Farid <moha.khamis@outlook.com>
2019-02-16 00:31:08 +0200

    Added Jennifer Aniston data cuz why not

--- routes/index.js
+++ routes/index.js
@@ -32,6 +32,25 @@ router.post('/results', function (req, res, next) {
             link: 'https://open.spotify.com/track/6K6OCvFQ4i7KKfGJsOAqY1?si=NfZeQLn6SQuZxmd7QRqt7g'
           }
         ]
+      },
+      {
+        name: 'Jane Doe',
+        picture: "https://bit.ly/2GxSeLJ",
+        bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum modi provident cum, praesentium ipsum reiciendis dignissimos animi eligendi totam, delectus culpa repudiandae voluptatibus? Consequatur ut voluptatum provident voluptate esse dignissimos.',
+        songs: [
+          {
+            title: 'Dancing Queen',
+            link: 'https://open.spotify.com/track/0GjEhVFGZW8afUYGChu3Rr?si=NgFSZq28QJKE_2yclyr5Sg'
+          },
+          {
+            title: 'Gimme Gimme',
+            link: 'https://open.spotify.com/track/3vkQ5DAB1qQMYO4Mr9zJN6?si=W5i-vu3YQ2uT-edfyvrOvw'
+          },
+          {
+            title: 'Wake Up',
+            link: 'https://open.spotify.com/track/6K6OCvFQ4i7KKfGJsOAqY1?si=NfZeQLn6SQuZxmd7QRqt7g'
+          }
+        ]
       }
     ]
   });
